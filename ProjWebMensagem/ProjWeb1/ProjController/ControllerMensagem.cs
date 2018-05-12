a8663110468d59ecb09163663c8c4b4756cb9b7c
vit0rrebello <37422650+vit0rrebello@users.noreply.github.com>
2018-05-11 21:14:17 -0300

    Add files via upload
    Mensagem

---
+++ ProjWebMensagem/ProjWeb1/ProjController/ControllerMensagem.cs
@@ -0,0 +1,63 @@
+﻿using ProjModel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjController
+{
+    public class ControllerMensagem
+    {
+
+        //ConnectionSQLServer conexao;
+        SqlConnection con;
+
+        public ControllerMensagem()
+        {
+            con = new SqlConnection
+                (ConfigurationManager.ConnectionStrings["SQLSERVER"].
+                ConnectionString);
+            //con.Open();
+        }
+
+        public bool Insert(Mensagem msg)
+        {
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "DBO.PR_INSERIR_MENSAGEM";
+            cmd.Parameters.Add(new SqlParameter
+                ("@Descricao", msg.Descricao));
+
+            con.Open();
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+
+            return true;
+        }
+
+        public DataTable Select ()
+        {
+            DataTable dt = new DataTable();
+            SqlCommand scomand = new SqlCommand();
+            SqlDataAdapter ad = new SqlDataAdapter();
+
+            con.Open();
+
+            scomand = new SqlCommand("PR_CONSULTAR_MENSAGEM", con);
+            scomand.CommandType = CommandType.StoredProcedure;
+            ad.SelectCommand = scomand;
+            ad.Fill(dt);
+
+            con.Close();
+
+            return dt;
+        }
+
+
+    }
+}
